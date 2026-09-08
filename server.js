f6b66af539080c0781125c7d6281b37e32271d1f
Your Name <your_email@example.com>
2026-09-09 01:52:09 +0530

    Fix Gmail SMTP port

--- server.js
+++ server.js
@@ -17,11 +17,16 @@ app.use(express.static("public"));
 // ===============================
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 
